3cea2895e8b1e37c5d7e5fa9de4fa273a22efd39
anshulparik <anshulparik@gmail.com>
2023-08-01 16:53:31 +0530

    made card component dynamic and flexible for all content type

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,30 @@
 import './Card.css';
+import { FiEdit } from 'react-icons/fi';
 
-import test from '../../../assets/test.jpg';
+interface CardProps {
+  title: string;
+  img: string;
+  location: string;
+  country: string;
+  description: string;
+}
 
-const Card = () => {
+const Card = ({ title, img, location, country, description }: CardProps) => {
   return (
     <div className="card-main">
       <div className="card-top">
-        <img className="card-img" src={test} alt="img" />
-        <span className="card-title">Mountain rides</span>
+        <img className="card-img" src={img} alt="img" />
+        <span className="card-title">{title}</span>
       </div>
       <div className="card-bottom">
-        <p className="card-desc">
-          A natural elevation of the earth's surface rising more or less
-          abruptly to a summit, and attaining an altitude greater than that of a
-          hill, usually greater than 2,000 feet (610 meters).
-        </p>
-        <p className="card-location">Kaza, India</p>
+        <p className="card-desc">{description}</p>
+        <div className="card-footer">
+          <FiEdit className="card-icon" />
+          <div className="card-location">
+            <p className="location">{location}</p>
+            <p className="country">{country}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,12 @@ import Sidebar from 'components/Sidebar/Sidebar';
 import Navbar from 'components/Navbar/Navbar';
 import Card from 'components/Card/Card';
 
+import test from '../../../assets/test.jpg';
+import test2 from '../../../assets/test2.jpg';
+import test3 from '../../../assets/test3.jpg';
+import test4 from '../../../assets/test4.jpg';
+import test5 from '../../../assets/test5.jpg';
+
 const Home = () => {
   return (
     <div className="home-main">
@@ -10,14 +16,52 @@ const Home = () => {
       {/* <Sidebar /> */}
       <div className="home-container">
         <div className="home-row">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card
+            title="Between the valley"
+            img={test}
+            description=" A natural elevation of the earth's surface rising more or less
+          abruptly to a summit, and attaining an altitude greater than that of a
+          hill, usually greater than 2,000 feet (610 meters)."
+            location="Munnar"
+            country="India"
+          />
+          <Card
+            title="Living the dream!"
+            img={test2}
+            description="A natural elevation of the earth's surface rising more or less
+          abruptly to a summit."
+            location="Ben Nevis"
+            country="Scotland"
+          />
+          <Card
+            title="Along the sky into the rails"
+            img={test3}
+            description=" A natural elevation of the earth's surface rising more or less
+          abruptly to a summit, and attaining an altitude greater than that of a
+          hill, usually greater than 2,000 feet (610 meters)."
+            location="thing vellir national park"
+            country="Democratic Republic of the Congo"
+          />
         </div>
         <div className="home-row">
-          <Card />
-          <Card />
+          <Card
+            title="Between the valley"
+            img={test5}
+            description=" A natural elevation of the earth's surface rising more or less
+          abruptly to a summit, and attaining an altitude greater than that of a
+          hill, usually greater than 2,000 feet (610 meters)."
+            location="Kinshasa"
+            country="Democratic Republic of the Congo"
+          />
+          <Card
+            title="Into the woods"
+            img={test4}
+            description=" A natural elevation of the earth's surface rising more or less
+          abruptly to a summit, and attaining an altitude greater than that of a
+          hill, usually greater than 2,000 feet (610 meters)."
+            location="Sarajevo"
+            country="Bosnia and Herzegovina"
+          />
         </div>
       </div>
     </div>

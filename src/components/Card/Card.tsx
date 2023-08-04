cb491acfd26c3c3fe5bfac484aec0e525296804b
anshulparik <anshulparik@gmail.com>
2023-08-04 15:59:38 +0530

    favorite, share and details icon added

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,20 @@
 import { CardProps } from 'utils/types';
 import './Card.css';
 import { FiEdit } from 'react-icons/fi';
+import { MdFavorite } from 'react-icons/md';
+import { FaShareFromSquare } from 'react-icons/fa6';
+import { BiSolidDetail } from 'react-icons/bi';
 
 const Card = ({ title, img, location, country, description }: CardProps) => {
   return (
     <div className="card-main">
       <div className="card-top">
         <img className="card-img" src={img} alt="img" />
+        <div className="card-img-icons">
+          <MdFavorite className="card-img-icon" />
+          <FaShareFromSquare className="card-img-icon" />
+          <BiSolidDetail className="card-img-icon" />
+        </div>
         <span className="card-title">{title}</span>
       </div>
       <div className="card-bottom">
